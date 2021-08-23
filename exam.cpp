7491856a7a756723a791f2485a90f9db757efa00
fcerda6 <>
2021-08-23 21:49:08 +0000

    message

---
+++ exam.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "My First Program is exam.cpp \n";
+}
+
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "My First Program in Repl.it \n";
+}
+
